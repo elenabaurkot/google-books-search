db7336a4dd31bd34acb6a35261a54f3b225dcf69
elenabaurkot <elenabaurkot@gmail.com>
2020-04-03 20:47:29 -0400

    frontend routes created

--- client/src/App.js
+++ client/src/App.js
@@ -16,7 +16,7 @@ const App = () => {
         </Switch>
       </React.Fragment>
     </Router>
-  )
-}
+  );
+};
 
 export default App;
---
+++ client/src/pages/Search.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+import Jumbotron from '../components/Jumbotron';
+import Container from '../components/Container';
+import Row from '../components/Row';
+import Column from '../components/Column';
+import Card from '../components/Card';
+import { searchGoogleBooks, saveBook, getSavedBooks } from '../utils/API';
